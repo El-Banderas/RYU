fab66d683b3d970d99a2530501390571851635c5
a93234@alunos.uminho.pt <DiogoPereiraMatos>
2023-03-10 15:01:03 +0000

    Topologias dos exercicios 1 e 2 (que vai ser feito de outra maneira)

--- topo.py
+++ topo.py
@@ -1,51 +1,64 @@
-"""Custom topology example
-
-Two directly connected switches plus a host for each switch:
-
-   host --- switch --- switch --- host
-
-Adding the 'topos' dict with a key/value pair to generate our newly defined
-topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+sudo mn --custom ~/topo.py --topo 1-2 --controller=remote
+--switch=ovsk --protocols=OpenFlow13 --mac
 """
 
 from mininet.topo import Topo
 
-class MyTopo( Topo ):
-    "Simple topology example."
+class Topo_1_1( Topo ):
+	"TP1, Exercicio 1"
 
-    def build( self ):
-        "Create custom topo."
+	def build( self ):
 
-        # Add hosts and switches
-        host1 = self.addHost( 'h1' )
-        host2 = self.addHost( 'h2' )
-        host3 = self.addHost( 'h3' )
-        host4 = self.addHost( 'h4' )
-        switch1 = self.addSwitch( 's1' )
-        # Add links
-        self.addLink( host1, switch1 )
-        self.addLink( host2, switch1 )
-        self.addLink( host3, switch1 )
-        self.addLink( host4, switch1 )
+		# Add hosts and switches
+		host1 = self.addHost( 'h1' )
+		host2 = self.addHost( 'h2' )
+		host3 = self.addHost( 'h3' )
+		host4 = self.addHost( 'h4' )
 
-		
-        host5 = self.addHost( 'h5' )
-        host6 = self.addHost( 'h6' )
-        host7 = self.addHost( 'h7' )
-        host8 = self.addHost( 'h8' )
-        switch2 = self.addSwitch( 's2' )
-        # Add links
-        self.addLink( host5, switch2 )
-        self.addLink( host6, switch2 )
-        self.addLink( host7, switch2 )
-        self.addLink( host8, switch2 )
+		switch1 = self.addSwitch( 's1' )
 
-	
-        switch3 = self.addSwitch( 's3' )
-        # Add links
-        self.addLink( switch1, switch3 )
-        self.addLink( switch2, switch3 )
+		# Add links
+		self.addLink( switch1, host1 )
+		self.addLink( switch1, host2 )
+		self.addLink( switch1, host3 )
+		self.addLink( switch1, host4 )
 
 
+class Topo_1_2( Topo ):
+	"TP1, Exercicio 2"
 
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+	def build( self ):
+
+		# Add hosts and switches
+		host1 = self.addHost( 'h1' )
+		host2 = self.addHost( 'h2' )
+		host3 = self.addHost( 'h3' )
+		host4 = self.addHost( 'h4' )
+		host5 = self.addHost( 'h5' )
+		host6 = self.addHost( 'h6' )
+		host7 = self.addHost( 'h7' )
+		host8 = self.addHost( 'h8' )
+
+		switch1 = self.addSwitch( 's1' )
+		switch2 = self.addSwitch( 's2' )
+		switch3 = self.addSwitch( 's3' )
+
+		# Add links
+		self.addLink( switch1, switch2 )
+		self.addLink( switch1, switch3 )
+		self.addLink( switch2, host1 )
+		self.addLink( switch2, host2 )
+		self.addLink( switch2, host3 )
+		self.addLink( switch2, host4 )
+		self.addLink( switch3, host5 )
+		self.addLink( switch3, host6 )
+		self.addLink( switch3, host7 )
+		self.addLink( switch3, host8 )
+
+
+
+topos = {
+	'1-1': ( lambda: Topo_1_1() ),
+	'1-2': ( lambda: Topo_1_2() )
+	}
