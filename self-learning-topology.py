8e689abf32d119db0ff687c4529d1dafae09ab25
a93234@alunos.uminho.pt <DiogoPereiraMatos>
2023-03-15 12:25:49 +0000

    a

--- self-learning-topology.py
+++ self-learning-topology.py
@@ -5,6 +5,9 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.topology import event
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet, ipv6
+from ryu.lib.packet import ether_types
 
 import sys
 #import ruamel.yaml
@@ -23,7 +26,41 @@ class SimpleSwitchLogger(app_manager.RyuApp):
 
 	def __init__(self, *args, **kwargs):
 		super(SimpleSwitchLogger, self).__init__(*args, **kwargs)
+
+		self.deprecated = {}
+		self.mac_to_port = {}
+
+		self.switches = {}
+		"""
+		switches:
+			- s1:
+				- 1: s1_mac1
+				- 2: s1_mac2
+				...
+			- s2:
+				...
+			...
+		"""
+
 		self.hosts = {}
+		"""
+		hosts:
+			- h1:
+				- ip: h1_ip
+				- mac: h1_mac
+			- h2:
+				...
+			...
+		"""
+		
+		self.connections = {}
+		"""
+		connections:
+			- s1_mac1: h1_mac1
+			- s1_mac2: h2_mac1
+			- s2_mac1: s1_mac3
+			...
+		"""
 
 	
 	@set_ev_cls(event.EventHostAdd)
@@ -33,9 +70,9 @@ class SimpleSwitchLogger(app_manager.RyuApp):
 		port_no = ev.host.port.port_no
 		mac = ev.host.mac
 
-		self.hosts.setdefault(dpid, {})
-		self.hosts[dpid][port_no] = {"mac":mac, "status":1}
-		print("host_add | hosts=", self.hosts)
+		self.deprecated.setdefault(dpid, {})
+		self.deprecated[dpid][port_no] = {"mac":mac, "status":1}
+		#print("host_add | deprecated=", self.deprecated)
 
 
 	@set_ev_cls(event.EventPortModify)
@@ -46,16 +83,41 @@ class SimpleSwitchLogger(app_manager.RyuApp):
 		port_no = ev.port.port_no
 		status = 1 if ev.port.is_live() else 0
 		# Se o estado anterior for igual ao atual, não modifica nada
-		if (self.hosts[dpid][port_no]["status"] == status):
+		if (self.deprecated[dpid][port_no]["status"] == status):
 			return 
-		self.hosts[dpid][port_no]["status"] = status
-		print("port_mod | hosts=", self.hosts)
+		self.deprecated[dpid][port_no]["status"] = status
+		#print("port_mod | deprecated=", self.deprecated)
 		#self.output_dict()
 	
 	
 	@set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
 	def _packet_in_handler(self, ev):
-		print("Hello!!")
+		msg = ev.msg
+		datapath = msg.datapath
+		ofproto = datapath.ofproto
+		parser = datapath.ofproto_parser
+		pkt = packet.Packet(msg.data)
+
+		eth = pkt.get_protocols(ethernet.ethernet)[0]
+		ipv = pkt.get_protocols(ipv6.ipv6)[0]
+
+		dpid = "s" + format(datapath.id, "d")
+		in_port = msg.match['in_port']
+		src_mac = eth.src
+		dst_mac = eth.dst
+		src_ip = ipv.src
+		dst_ip = ipv.dst
+
+		self.mac_to_port.setdefault(dpid, {})
+		self.mac_to_port[dpid][src_mac] = in_port
+
+		if dst_mac in self.mac_to_port[dpid]:
+			out_port = self.mac_to_port[dpid][dst_mac]
+		else:
+			out_port = -1
+			
+		self.logger.info("packet_in | dpid=%s src_ip=%s dst_ip=%s src_mac=%s dst_mac=%s in_port=%s out_port=%s", dpid, src_ip, dst_ip, src_mac, dst_mac, in_port, out_port)
+		#print("packet_in | mac_to_port=", self.mac_to_port)
 
 
 	def output_dict(self):
@@ -63,7 +125,7 @@ class SimpleSwitchLogger(app_manager.RyuApp):
 		print("YAML")
 		dic_final = {}
 		current_entryes_number = 0
-		for switch_name, switch_devices in self.hosts.items():
+		for switch_name, switch_devices in self.deprecated.items():
 			dic_final[switch_name] = clean_switch(switch_devices)
 		print("Final")
 		print(dic_final)
@@ -98,4 +160,5 @@ class SimpleSwitchLogger(app_manager.RyuApp):
 	@set_ev_cls(event.EventHostDelete)
 	@set_ev_cls(event.EventHostMove)
 	def _print_event_default(self, ev):
-		print(str(ev))
+		#print(str(ev))
+		return
