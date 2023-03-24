b7d7793cd510ae5e9c8d27123930d9159d9d42a4
a93234@alunos.uminho.pt <DiogoPereiraMatos>
2023-03-24 15:59:41 +0000

    mininet com controllers em portas definidas

--- tp2/topo_.py
+++ tp2/topo_.py
@@ -1,12 +1,18 @@
 
 from mininet.net import Mininet
-from mininet.node import Controller, OVSSwitch
+from mininet.node import Controller, OVSSwitch, RemoteController
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 
+"""
+ryu-manager ~/switchVLAN.py --ofp-tcp-listen-port 6633
+ryu-manager ~/LoadBalancer.py --ofp-tcp-listen-port 6634
+sudo python2.7 -E topo.py
+"""
+
 def TP2():
 
-	net = Mininet( controller=Controller, switch=OVSSwitch, waitConnected=True )
+	net = Mininet( controller=RemoteController, switch=OVSSwitch, waitConnected=True )
 
 	info( "*** Creating controllers\n" )
 	c1 = net.addController( 'switchVLAN', port=6633 )
@@ -14,30 +20,30 @@ def TP2():
 
 
 	info( "*** Floor 3\n" )
-	host31 = net.addHost( 'h31', ip='10.0.10.31/24', mac='00:00:00:00:00:31' )
-	host32 = net.addHost( 'h32' , ip='10.0.20.32/24', mac='00:00:00:00:00:32')
-	host33 = net.addHost( 'h33' , ip='10.0.30.33/24', mac='00:00:00:00:00:33')
+	host31 = net.addHost( 'h31', ip='10.0.10.31/16', mac='00:00:00:00:00:31' )
+	host32 = net.addHost( 'h32' , ip='10.0.20.32/16', mac='00:00:00:00:00:32')
+	host33 = net.addHost( 'h33' , ip='10.0.30.33/16', mac='00:00:00:00:00:33')
 
 	switch3 = net.addSwitch( 's3' )
 
 	info( "*** Floor 2\n" )
-	host21 = net.addHost( 'h21' , ip='10.0.10.21/24', mac='00:00:00:00:00:21')
-	host22 = net.addHost( 'h22' , ip='10.0.20.22/24', mac='00:00:00:00:00:22')
-	host23 = net.addHost( 'h23' , ip='10.0.30.23/24', mac='00:00:00:00:00:23')
+	host21 = net.addHost( 'h21' , ip='10.0.10.21/16', mac='00:00:00:00:00:21')
+	host22 = net.addHost( 'h22' , ip='10.0.20.22/16', mac='00:00:00:00:00:22')
+	host23 = net.addHost( 'h23' , ip='10.0.30.23/16', mac='00:00:00:00:00:23')
 
 	switch2 = net.addSwitch( 's2' )
 
 	info( "*** Floor 1\n" )
-	host11 = net.addHost( 'h11' , ip='10.0.10.11/24', mac='00:00:00:00:00:11')
-	host12 = net.addHost( 'h12' , ip='10.0.20.12/24', mac='00:00:00:00:00:12')
-	host13 = net.addHost( 'h13' , ip='10.0.30.13/24', mac='00:00:00:00:00:13')
+	host11 = net.addHost( 'h11' , ip='10.0.10.11/16', mac='00:00:00:00:00:11')
+	host12 = net.addHost( 'h12' , ip='10.0.20.12/16', mac='00:00:00:00:00:12')
+	host13 = net.addHost( 'h13' , ip='10.0.30.13/16', mac='00:00:00:00:00:13')
 
 	switch1 = net.addSwitch( 's1' )
 
 	info( "*** Servers\n" )
-	server1 = net.addHost( 'h01' , ip='10.0.10.101/24', mac='00:00:00:00:01:01')
-	server2 = net.addHost( 'h02' , ip='10.0.10.102/24', mac='00:00:00:00:01:02')
-	server3 = net.addHost( 'h03' , ip='10.0.10.103/24', mac='00:00:00:00:01:03')
+	server1 = net.addHost( 'h01' , ip='10.0.10.101/16', mac='00:00:00:00:01:01')
+	server2 = net.addHost( 'h02' , ip='10.0.10.102/16', mac='00:00:00:00:01:02')
+	server3 = net.addHost( 'h03' , ip='10.0.10.103/16', mac='00:00:00:00:01:03')
 
 	switch0 = net.addSwitch( 's0' )
 
